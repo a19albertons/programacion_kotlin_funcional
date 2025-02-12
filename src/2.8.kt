8d2549ff52e81d63f485f61c6fdf836471b05f0a
a19albertons <a19albertons@iessanclemente.net>
2025-02-12 17:30:37 +0100

    Actualizado 11/02/2025 Funciones high order con colecciones hasta el 10 y correciones hasta el 9 el 8 es parcial

--- src/2.8.kt
+++ src/2.8.kt
@@ -1,26 +1,14 @@
-data class moda (val numero: Int, val veces: Int)
 fun main(){
 
 
-    generateSequence {
-        val num_numeros= readln().toInt()
-        if (num_numeros == 0) {
-            null
+    generateSequence(::readln)
+        .map { it.toInt() }
+        .takeWhile { it!=0 }
+        .forEach {
+            val lista_numero= readln().split(" ").map { it.toInt() }
+            val procesado=lista_numero.groupBy { it }.maxBy { it.value.size }
+            println(procesado.key)
         }
-        else {
-            num_numeros
-        }
-    }.forEach {
-        val lista_moda = mutableListOf<moda>()
-        val lista_numero= readln().split(" ").map { it.toInt() }
-        lista_numero.distinct().forEach { distinguido ->
-            val veces = lista_numero.filter { it == distinguido }.count()
-            lista_moda.add(moda(distinguido, veces))
-        }
-        lista_moda.filter { it.veces == lista_moda.maxOf { it.veces } }.forEach{
-            println(it.numero)
-        }
-    }
 
 
 }
